56afdcb5f24c99283d1b92e1f5c0f63f53ad4153
andyols <andyolson09@gmail.com>
2019-03-06 16:07:28 -0600

    complete

--- src/index.js
+++ src/index.js
@@ -1,38 +1,43 @@
-import add from "./modules/add";
-import subtract from "./modules/subtract";
-import multiply from "./modules/multiply";
-import {addTen, addFive} from "./modules/addNumbers";
-import {subtractOne} from "./modules/subtractNumbers";
+import add from './modules/add'
+import subtract from './modules/subtract'
+import multiply from './modules/multiply'
+import divide from './modules/divide'
+import { addTen, addFive, addOne } from './modules/addNumbers'
+import {
+  subtractOne,
+  subtractFive,
+  subtractTen
+} from './modules/subtractNumbers'
 
-const number1 = 393;
-const number2 = 238;
+const number1 = 393
+const number2 = 238
 
-let answer = add(number1,number2);
-console.log(answer);
+let answer = add(number1, number2)
+console.log(answer)
 
-answer = subtract(number1,number2);
-console.log(answer);
+answer = subtract(number1, number2)
+console.log(answer)
 
-answer = multiply(number1,number2);
-console.log(answer);
+answer = multiply(number1, number2)
+console.log(answer)
 
-answer = divide(number1,number2);
-console.log(answer);
+answer = divide(number1, number2)
+console.log(answer)
 
-answer = addTen(number1);
-console.log(answer);
+answer = addTen(number1)
+console.log(answer)
 
-answer = addFive(number1);
-console.log(answer);
+answer = addFive(number1)
+console.log(answer)
 
-answer = addOne(number1);
-console.log(answer);
+answer = addOne(number1)
+console.log(answer)
 
-answer = subtractOne(number1);
-console.log(answer);
+answer = subtractOne(number1)
+console.log(answer)
 
-answer = subtractFive(number1);
-console.log(answer);
+answer = subtractFive(number1)
+console.log(answer)
 
-answer = subtractTen(number1);
-console.log(answer);
+answer = subtractTen(number1)
+console.log(answer)
